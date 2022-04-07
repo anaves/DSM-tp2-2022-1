f64b503a770127508a51ec35028896a5b1d21e71
anaves <anaves@gmail.com>
2022-04-07 14:12:39 -0300

    Aula 6abr - modificadores de acesso

--- Aula06abr/src/Conta.java
+++ Aula06abr/src/Conta.java
@@ -1,9 +1,48 @@
 public class Conta {
     private double saldo;
+    private double limite;
     
-    // metodo de acesso
-    public double getSaldo(){
-        return this.saldo;
+    // apenas o metodo de consulta saldo
+    public double getSaldo() {
+        return saldo;
     }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+
+        if (limite >= 0){
+            this.limite = limite;
+        }
+    }
+    private void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public boolean sacar(double valor) {
+        if(valor <= this.getLimite()+this.getSaldo()){
+            double novoSaldo = this.getSaldo()-valor;
+            //this.saldo = novoSaldo;
+            this.setSaldo(novoSaldo); // usando o metodo privado
+            return true;
+        }
+        return false;
+    }
+
+    public boolean depositar(double valor) {
+        if (valor > 0){
+            double saldoAtual = this.getSaldo();
+            this.setSaldo(saldoAtual+valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    
+    
+    
     
 }
--- Aula06abr/src/TesteConta.java
+++ Aula06abr/src/TesteConta.java
@@ -2,7 +2,27 @@ public class TesteConta {
     public static void main(String[] args) {
         Conta c = new Conta();
         //c.saldo = 5000; // nao devemos permitir isto!
-        System.out.println("Saldo: R$ " + c.getSaldo());
+        c.setLimite(1000);
+        if(c.depositar(10)){
+            System.out.println("Deposito com sucesso");
+        }else{
+            System.out.println("Deposito nao realizado");
+        }
+
+        System.out.println("Saldo antes: R$ " + c.getSaldo());
+        if (c.sacar(990)){
+            System.out.println("Saque realizado");
+            System.out.println("Saldo atual R$ " + c.getSaldo());
+        }else{
+            System.out.println("Saque nao foi permitido");
+        }
+
+        if (c.sacar(15)){
+            System.out.println("Saque realizado");
+            System.out.println("Saldo atual R$ " + c.getSaldo());
+        }else{
+            System.out.println("Saque nao foi permitido");
+        }
         
     }
 }
